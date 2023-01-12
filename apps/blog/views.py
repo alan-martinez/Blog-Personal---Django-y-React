fdc21da9b9bb538ab26fcfca39aeac43ee5515c2
Alan Jahir <alan.martinez5691@alumnos.udg.mx>
2023-01-12 12:23:15 -0600

    Paginacion completada

--- apps/blog/views.py
+++ apps/blog/views.py
@@ -7,6 +7,7 @@ from .models import Post, ViewCount
 from apps.category.models import Category
 
 from .serializers import PostListSerializer
+from .paginacion import SmallSetPagination, MediumSetPagination, LargeSetPagination
 
 class BlogListView(APIView):
     permission_classes = (permissions.AllowAny,)
@@ -14,8 +15,12 @@ class BlogListView(APIView):
         if Post.objects.all().exists():
             
             posts = Post.objects.all()
-            serializer = PostListSerializer(posts, many=True)
+
+            paginator = SmallSetPagination()
+            result = paginator.paginate_queryset(posts, request)
+
+            serializer = PostListSerializer(result, many=True)
             
-            return Response({'posts':serializer.data}, status=status.HTTP_200_OK)
+            return paginator.get_paginated_response({'posts':serializer.data})
         else:
             return Response({'error':'not post found'}, status=status.HTTP_404_NOT_FOUND)
